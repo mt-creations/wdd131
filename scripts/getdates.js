47c7a7f6710b2acc8b29d87bb945a292e1219978
mt-creations <mthomas92551@gmail.com>
2026-01-10 20:56:27 -0800

    homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentYear").textContent = `©️${currentYear} 🌴 Michael Thomas 🌴 California`;
+
+document.getElementById("lastModified").textContent = `Last modified: ${document.lastModified}`;
