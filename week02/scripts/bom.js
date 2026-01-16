1b3bbb24c7d743b75cc4e95b0d64796e3e7f43f1
mt-creations <mthomas92551@gmail.com>
2026-01-15 16:50:00 -0800

    week 2 activity 4

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,21 +1,32 @@
 
 
-
 const input = document.querySelector('#favchap');
-
 const button = document.querySelector('button');
+const list = document.querySelector('#list');
 
-const list = document.querySelector('list');
 
-const li = document.createElement('li');
+button.addEventListener('click', function() {
+    if (input.value.trim() === '') {
+        input.focus();
+        return
+    }
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
 
-const deleteButton = document.createElement('button');
+    li.appendChild(deleteButton);
+    list.append(li);
 
-li.textContent = input.value;
+    deleteButton.addEventListener('click', function() {
+    list.removeChild(li);
+    input.focus();
+    
+    });
 
-deleteButton.textContent = '❌'
+    input.value = '';
+    input.focus();
 
-li.appendChild(deleteButton);
+});
 
-list.append(li);
 
