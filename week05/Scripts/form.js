0a8932490bfed6363427b036cf4bf7fedf8ba661
mt-creations <mthomas92551@gmail.com>
2026-02-07 15:02:32 -0800

    product review

---
+++ week05/Scripts/form.js
@@ -0,0 +1,44 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentYear").textContent = `©️${currentYear} 🌴 Michael Thomas 🌴 California`;
+
+document.getElementById("lastModified").textContent = `Last modified: ${document.lastModified}`;
+
+// Product Array
+
+const products = [
+    {
+        id: "fc-1888",
+         name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+  ];
+
+const select = document.getElementById("product");
+
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.calue = product.name;
+    option.textContent = product.name
+    select.appendChild(option);
+});
