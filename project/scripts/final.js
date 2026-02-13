b1eff341ab19d8b9f274814e317c26aa3eba8e54
mt-creations <mthomas92551@gmail.com>
2026-02-12 23:34:07 -0800

    final

--- project/scripts/final.js
+++ project/scripts/final.js
@@ -11,3 +11,35 @@ const navLinks = document.querySelector('.navigation');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
 });
+
+
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const selectContact = document.querySelector("input[name='contactType']:checked");
+    let contactValue;
+
+    if (selectContact !== null) {
+        contactValue = selectContact.value
+    }
+    else {
+        contactValue = "Not specified";
+    }
+
+    const formData = {
+        name: form.fullName.value,
+        phone: form.phone.value,
+        address: form.address.value, 
+        email:form.email.value,
+        newsletter: form.newsletter.checked,
+        contactType: contactValue
+    };
+
+    localStorage.setItem("contactSubmission", JSON.stringify(formData));
+
+    form.innerHTML = 
+        `<h2>Thank You, ${formData.name}</h2>
+        <p>We received your ${formData.contactType.toLowerCase()}.  We are so happy that you reached out to us!</p>`;
+});
